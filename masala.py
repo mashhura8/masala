af84bbc562d93989ea3e249b412542a1a6d73cea
mashhura8 <mashhurakarimboyeva8@gmail.com>
2025-10-03 16:31:50 +0500

    m

--- masala.py
+++ masala.py
@@ -1,5 +1,88 @@
 # 1-masala
-fam = input("Familiyangizni kiriting: ").capitalize()
-ism = input("Ismingizni kiriting: ").capitalize()
-shar = input("Sharifingizni kiriting: ").capitalize()
-print(f"F.I.Sh: {fam} {ism} {shar}")
+# fam = (input("Familiyangizni kiriting: ").capitalize()
+# ism = input("Ismingizni kiriting: ").capitalize()
+# shar = input("Sharifingizni kiriting: ").capitalize()
+# print(f"F.I.Sh: {fam} {ism} {shar}")
+
+
+# Pythonda arifmetik operatorlar mavzusida algoritmik masalalar
+# 1-masala
+a = int(input("Birinchi sonni kiriting: "))
+b = int(input("Ikkinchi sonni kiriting: "))
+sum = a + b
+print(f"Yig'indi  {sum}")
+
+
+# 2-masala
+l = int(input("To'rtburchakning yuzini kiriting: "))
+w = int(input("To'rtburchakning enini kiriting: "))
+area = l * w
+print(f"To'rburchakning yuzi {area}" )
+
+
+# 3-masala
+x = int(input("Birinchi sonni kiriting: "))
+y = int(input("Ikkinchi sonni kiriting: "))
+z = int(input("Uchinchi sonni kiriting: "))
+average = (x + y + z) / 2
+print(f"O'rta arifmetigi: {average}")
+
+
+# 4-masala
+a = int(input("Sonni kiriting: "))
+b =  int(input("Sonni kiriting: "))
+p = 2 * (a + b)
+print(f"To'rtburchak perimetr: {p}")
+
+
+# 5-masala
+h = int(input("Soatni kiriting: "))
+m = int(input("Daqiqani kiriting: "))
+total_min = h * 60 + m
+print(f"Umumiy daqiqa: {total_min}")
+
+
+# 6-masala
+usd = int(input("Dollar miqdorini kiriting: "))
+sum =  usd * 12000
+print(f"So'm: {sum}")
+
+
+# 7-masala
+a = int(input("Birinchi sonni kiriting: "))
+b = int(input("Ikkinchi sonni kiriting: "))
+diff = abs(a - b)
+print(f"Modul: {diff}")
+
+
+# 8-masala
+x = int(input("Birinchi sonni kiriting: "))
+n = int(input("Ikkinchi sonni kiriting: "))
+power = x ** n
+print(f"Darajasi: {power}")
+
+
+# 9-masala
+a = int(input("Birinchi sonni kiriting: "))
+b = int(input("Ikkinchi sonni kiriting: "))
+quotient = a // b
+print(f"Haqiqiy: {quotient}")
+
+
+# 10-masala
+a = int(input("Birinchi sonni kiriting: "))
+b = int(input("Ikkinchi sonni kiriting: "))
+a, b = b, a
+print(f"Almashtirish {a, b }")
+
+
+
+
+
+
+
+
+
+
+
+
