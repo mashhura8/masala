6f298fc8382a884bd1175a153da5607844d72a00
mashhura8 <mashhurakarimboyeva8@gmail.com>
2025-10-03 15:40:32 +0500

    n

--- masala.py
+++ masala.py
@@ -1,26 +1,5 @@
 # 1-masala
-fam = "Eshmatov "
-ism = "Toshmat "
-shar = "Toshpolatovich "
-print(fam+ism+shar)
 fam = input("Familiyangizni kiriting: ").capitalize()
 ism = input("Ismingizni kiriting: ").capitalize()
 shar = input("Sharifingizni kiriting: ").capitalize()
 print(f"F.I.Sh: {fam} {ism} {shar}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
